e8afe6f951a0bade24eaa6d0dc1923e15042cea8
keironjerome <Keyjerome@gmail.com>
2020-11-25 10:12:11 -0500

    removed those classes that are not required causing troubles running the app I think. can reverse later

--- src/main/java/com/udacity/bootstrap/repository/DogRepository.java
+++ src/main/java/com/udacity/bootstrap/repository/DogRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface DogRepository extends CrudRepository<Dog, Long> {
 
-
+/*
     @Query("select d.id, d.breed from Dog d where d.id=:id")
     String findBreedById(Long id);
 
@@ -20,5 +20,6 @@ public interface DogRepository extends CrudRepository<Dog, Long> {
 
     @Query("select d.id, d.origin from Dog d")
     List<String> findAllOrigin();
+*/
 
 }
